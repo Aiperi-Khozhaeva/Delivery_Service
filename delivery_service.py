f1778fa7a817001643f6f11a53fa6f852895d4f6
Aiperi <aiperi.khozhaeva@gmail.com>
2024-10-07 16:50:58 +0300

    Third iteration, changed

--- delivery_service.py
+++ delivery_service.py
@@ -6,11 +6,11 @@ import sys
 def delivery_service(robots_weight: list[int], limit: int) -> int:
     robots_weight = sorted(robots_weight)
     count: int = 0
-    x, y = 0, len(robots_weight) - 1
-    while x <= y:
-        if robots_weight[x] + robots_weight[y] <= limit:
-            x += 1
-        y -= 1
+    left_pointer, right_pointer = 0, len(robots_weight) - 1
+    while left_pointer <= right_pointer:
+        if robots_weight[left_pointer] + robots_weight[right_pointer] <= limit:
+            left_pointer += 1
+        right_pointer -= 1
         count += 1
     return count
 
